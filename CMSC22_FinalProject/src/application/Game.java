12896c1204047bca9b201f550de571586652305c
RavenKape02 <jfcaduyac@up.edu.ph>
2024-11-24 19:06:15 +0800

    First Commit

---
+++ CMSC22_FinalProject/src/application/Game.java
@@ -0,0 +1,94 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.geometry.Pos;
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class Game {
+	private Scene scene;
+    private Parent root;
+    private Canvas canvas;
+    private Stage stage;
+
+    public static final int WINDOW_WIDTH = 568;
+    public static final int WINDOW_HEIGHT = 568;
+    
+    public Game() {
+        this.root = new Group(); // Group for general scene handling
+        this.scene = new Scene(root); // Attach the root to the scene
+        this.canvas = new Canvas(WINDOW_WIDTH, WINDOW_HEIGHT); // Create canvas
+        this.stage = new Stage();
+        stage.setScene(scene);
+        ((Group) this.root).getChildren().add(this.canvas); // Add the canvas to the group root
+    }
+    
+    public void startGame() {
+    	this.stage.show();
+    }
+    
+    public void mainMenu() {
+    	StackPane mainMenu = new StackPane();
+    	VBox menuLayout = new VBox(20);
+    	
+    	Button newgame = new Button("New Game");
+    	Button about = new Button("About");
+    	Button developers = new Button("Developers");
+    	
+    	StackPane.setAlignment(newgame, Pos.BOTTOM_LEFT);
+    	StackPane.setAlignment(about, Pos.BOTTOM_CENTER);
+    	StackPane.setAlignment(developers, Pos.BOTTOM_RIGHT);
+    	
+    	newgame.setOnAction(e->{
+    		//function for starting a game
+    	});
+    	
+    	about.setOnAction(e->{
+    		aboutPage();
+    	});
+    	
+    	developers.setOnAction(e->{
+    		//developers page
+    	});
+    	
+    	mainMenu.getChildren().addAll(newgame, about, developers);
+    	
+    	
+    	this.root = new Group();
+    	this.scene = new Scene(mainMenu, 1280, 720);
+    	stage.setScene(scene);
+    	this.stage.show();
+    }
+    
+    public void aboutPage() {
+    	StackPane about = new StackPane();
+    	
+    	Text aboutText = new Text("This is just a simple shoot em up game");
+    	aboutText.setFont(Font.font("Showcard Gothic", 20));
+    	about.getChildren().add(aboutText);
+    	StackPane.setAlignment(aboutText, Pos.TOP_LEFT);
+    	
+    	Button goBack = new Button("Go Back to Main Menu");
+    	goBack.setAlignment(Pos.BOTTOM_LEFT);
+    	about.getChildren().add(goBack);
+    	StackPane.setAlignment(goBack, Pos.BOTTOM_LEFT);
+    	
+    	goBack.setOnAction(e->{
+    		mainMenu();
+    	});
+    	
+    	this.scene = new Scene(about, 1280, 720);
+    	stage.setScene(scene);
+    	this.stage.show();
+    }
+}
